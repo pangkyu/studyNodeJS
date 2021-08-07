7b1b4dc01aa7abd7ded35b5dbee6b40b94f506c3
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-08-07 15:08:49 +0900

    20210807

--- main.js
+++ main.js
@@ -2,43 +2,70 @@ const http = require('http');
 const fs = require("fs");
 const url = require('url');
 const app = http.createServer(function (request, response){
-    const _url = request.url;
-    const queryData = url.parse(_url, true).query;
-    const title = queryData.id;
-  
-    if(_url == "/"){
-        title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
+const _url = request.url;
+const queryData = url.parse(_url, true).query;
+const pathname = url.parse(_url, true).pathname;
+
+
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            
+            fs.readFile(`data/${queryData.id}`, `utf-8`, function(err, description){
+                const title = 'Welcome';
+                var description = 'hello, node JS';
+                const template = `
+                <!doctype html>
+                <html>
+                <head>
+                 <title>WEB1 - ${title} </title>
+                 <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ul>
+                <h2> ${title} </h2>
+                <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+             });
+        }else{
+            fs.readFile(`data/${queryData.id}`, `utf-8`, function(err, description){
+                const title = queryData.id;
+                const template = `
+                <!doctype html>
+                <html>
+                <head>
+                 <title>WEB1 - ${title} </title>
+                 <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ul>
+                <h2> ${title} </h2>
+                <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+             });
+        }
+       
+    }else{
         response.writeHead(404);
-        response.end();
-        return;
+        response.end('not found');
     }
-    response.writeHead(200);
-    const template = `
-    <!doctype html>
-    <html>
-    <head>
-     <title>WEB1 - ${title} </title>
-     <meta charset="utf-8">
-    </head>
-    <body>
-    <h1><a href="/">WEB</a></h1>
-    <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ul>
-    <h2> ${title} </h2>
-    <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-    <img src="coding.jpg" width="100%">
-    </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-    </p>
-    </body>
-    </html>
-    `;
-   
-    response.end(template);
 
 });
 app.listen(3000);
