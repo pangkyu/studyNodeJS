a066587916a777377ed17d4c86747b54c89f88ec
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-11-17 23:50:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,49 @@
-var http = require('http');
+const express = require('express');
+const template = require('./lib/template');
+const fs = require('fs');
+const path = require('path');
+const sanitizeHtml = require('sanitize-html');
+const app = express();
+
+app.get('/', (req,res) => 
+  fs.readdir('./data', function(error, filelist){
+    const title = 'welcome';
+    const description = 'hello, nodejs';
+    const list = template.list(filelist);
+    const html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href = "/create">create</a>`);
+    res.send(html);
+  })
+);
+
+app.get('/page/:pageId', (req,res) => 
+  fs.readdir('./data', function(error, filelist){
+    const filteredId = path.parse(req.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      const title = req.params.pageId;
+      const sanitizedTitle = sanitizeHtml(title);
+      const sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ['h1']
+      });
+      const list = template.list(filelist);
+      const html = template.HTML(sanitizedTitle, list, 
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        `<a href = "/create">create</a>
+         <a href = "/update?id=${sanitizedTitle}">update</a>
+         <form action = "delete_process" method = "post">
+          <input type = "hidden" name = "id" value = "${sanitizedTitle}">
+          <input type = "submit" value = "delete">
+         </form>`);
+         res.send(html);
+    })
+  }) 
+);
+
+app.listen(3000, () => console.log('example app listening on port 3000!'));
+
+
+/*var http = require('http');
 var url = require('url');
 var topic = require('./lib/topic');
 var author = require('./lib/author');
@@ -38,4 +83,4 @@ var app = http.createServer(function(request,response){
       response.end('Not found');
     }
 });
-app.listen(3000);
+app.listen(3000);*/
